f6d6cd842c9e27fc000c48fd1911f3ecc9d1b048
Angus Yeung <afyeung@gmail.com>
2018-11-16 13:55:13 -0800

    Removed Stencil Template Engine
    Removed the dependence of Stencil template engine.

--- ch11_API_Gateway_Kitura/myJournal/Package.swift
+++ ch11_API_Gateway_Kitura/myJournal/Package.swift
@@ -9,7 +9,6 @@ let package = Package(
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "8.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
-      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.8.0"),
       .package(url: "https://github.com/IBM-Swift/Swift-Kuery-ORM.git", .upToNextMinor(from: "0.3.1")),
       .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git", from: "1.2.0"),
     ],
