8d86f99f9bd3afe55cb1988e3730752856978fc1
Angus Yeung <afyeung@gmail.com>
2018-11-19 19:38:41 -0800

    Updated JournalRoutes
    (1) Improved the route path using GET/POST/PUT/DELETE
    (2) Separated out the creation of new Entry instance in a new line
    (3) Added comment tags

--- ch05_Routes_Vapor/myJournal/Sources/App/Routes/JournalRoutes.swift
+++ ch05_Routes_Vapor/myJournal/Sources/App/Routes/JournalRoutes.swift
@@ -2,38 +2,40 @@ import Vapor
 
 struct JournalRoutes : RouteCollection {
     
-    let journal = JournalController()
+    let journal = JournalController()               // [1]
     
     func boot(router: Router) throws {
         
-        let topRouter = router.grouped("journal")
-        topRouter.get("total", use: getTotal)
-        topRouter.post("new", use: newEntry)
+        let topRouter = router.grouped("journal")   // [2]
+        topRouter.get(use: getTotal)
+        topRouter.post(use: newEntry)
         
-        let entryRouter = router.grouped("journal", Int.parameter)
-        entryRouter.get("get", use: getEntry)
-        entryRouter.post("edit", use: editEntry)
-        entryRouter.get("remove", use: removeEntry)
+        let entryRouter = router.grouped("journal", Int.parameter) // [3]
+        entryRouter.get(use: getEntry)
+        entryRouter.put(use: editEntry)
+        entryRouter.delete(use: removeEntry)
     }
     
-    func getTotal(_ req: Request) -> String {
+    func getTotal(_ req: Request) -> String {   // [4]
         let total = journal.total()
+        print("Total Records: \(total)")
         return "\(total)"
     }
     
-    func newEntry(_ req: Request) throws -> Future<HTTPStatus> {
+    func newEntry(_ req: Request) throws -> Future<HTTPStatus> {    // [5]
         let newID = UUID().uuidString
         return try req.content.decode(Entry.self).map(to: HTTPStatus.self) { entry in
-            if let result = self.journal.create(Entry(id: newID,
-                                                      title: entry.title,
-                                                      content: entry.content)) {
+            let newEntry = Entry(id: newID,
+                                 title: entry.title,
+                                 content: entry.content)
+            if let result = self.journal.create(newEntry) {
                 print("Created: \(result)")
             }
             return .ok
         }
     }
     
-    func getEntry(_ req: Request) throws -> Entry {
+    func getEntry(_ req: Request) throws -> Entry {     // [6]
         let index = try req.parameters.next(Int.self)
         print("Index = \(index)")
         let res = req.makeResponse()
@@ -45,21 +47,21 @@ struct JournalRoutes : RouteCollection {
         return Entry(id: "-1")
     }
     
-    func editEntry(_ req: Request) throws -> Future<HTTPStatus> {
+    func editEntry(_ req: Request) throws -> Future<HTTPStatus> {      // [7]
         let index = try req.parameters.next(Int.self)
         let newID = UUID().uuidString
         return try req.content.decode(Entry.self).map(to: HTTPStatus.self) { entry in
-            if let result = self.journal.update(index: index,
-                                          entry: Entry(id: newID,
-                                                       title: entry.title,
-                                                       content: entry.content)) {
+            let newEntry = Entry(id: newID,
+                                 title: entry.title,
+                                 content: entry.content)
+            if let result = self.journal.update(index: index, entry: newEntry) {
                 print("Updated: \(result)")
             }
             return .ok
         }
     }
 
-    func removeEntry(_ req: Request) throws -> HTTPStatus {
+    func removeEntry(_ req: Request) throws -> HTTPStatus {     // [8]
         let index = try req.parameters.next(Int.self)
         if let result = self.journal.delete(index: index) {
             print("Deleted: \(result)")
