67d42acf5162ff277f7300830cc4af6549d3989b
Angus Yeung <afyeung@gmail.com>
2018-11-19 23:32:35 -0800

    Updated JournalController
    Updated JournalController for index bounds.

--- ch05_Routes_Kitura/myJournal/Sources/Application/Controllers/JournalController.swift
+++ ch05_Routes_Kitura/myJournal/Sources/Application/Controllers/JournalController.swift
@@ -9,10 +9,10 @@ import Foundation
 
 final class JournalController {
     
-    var entries : Array<Entry> = Array()
+    var entries : Array<Entry> = Array()  
     
     //: Get total number of entries
-    func total() -> Int {
+    func total() -> Int {                   
         return entries.count
     }
     //: Create a new journal entry
@@ -21,28 +21,34 @@ final class JournalController {
         return entries.last
     }
     //: Read a journal entry
-    func read(index: Int) -> Entry? {
-        if index < entries.count {
-            return entries[index]
-        } else {
-            return nil
-        }
+    func read(index: Int) -> Entry? {      
+        if let entry = entries.get(index: index) {
+            return entry
+        } 
+        return nil
     }
     //: Update the journal entry
-    func update(index: Int, entry: Entry) -> Entry? {
-        if index < entries.count {
+    func update(index: Int, entry: Entry) -> Entry? {   
+        if let entry = entries.get(index: index) {
             entries[index] = entry
             return entry
-        } else {
-            return nil
-        }
+        } 
+        return nil
     }
     //: Delete a journal entry
-    func delete(index: Int) -> Entry? {
-        if index < entries.count {
+    func delete(index: Int) -> Entry? {    
+        if let _ = entries.get(index: index) {
             return entries.remove(at: index)
-        } else {
-            return nil
+        } 
+        return nil
+    }
+}
+
+extension Array {
+    func get(index: Int) -> Element? {
+        if index >= 0 && index < count {
+            return self[index]
         }
+        return nil
     }
 }
