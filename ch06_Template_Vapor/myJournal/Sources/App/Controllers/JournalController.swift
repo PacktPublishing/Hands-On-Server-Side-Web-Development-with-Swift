2d084874eb65c60821418e56df208b32854464a7
Angus Yeung <afyeung@gmail.com>
2018-11-21 14:07:21 -0800

    Updated JournalController
    (1) Updated the code to incorporate the changes in Ch5 version of myJournal.
    (2) Added a new function readAll().

--- ch06_Template_Vapor/myJournal/Sources/App/Controllers/JournalController.swift
+++ ch06_Template_Vapor/myJournal/Sources/App/Controllers/JournalController.swift
@@ -9,45 +9,52 @@ import Vapor
 
 final class JournalController {
     
-    var entries : Array<Entry> = Array()
+    var entries : Array<Entry> = Array()    // [1]
     
     //: Get total number of entries
-    func total() -> Int {
+    func total() -> Int {                   // [2]
         return entries.count
     }
     //: Create a new journal entry
-    func create(_ entry: Entry) -> Entry? {
+    func create(_ entry: Entry) -> Entry? { // [3]
         entries.append(entry)
         return entries.last
     }
     //: Read a journal entry
-    func read(index: Int) -> Entry? {
-        if index < entries.count {
-            return entries[index]
-        } else {
-            return nil
-        }
+    func read(index: Int) -> Entry? {       // [5]
+        if let entry = entries.get(index: index) {
+            return entry
+        } 
+        return nil
     }
     //: Read all journal entries
     func readAll() -> [Entry] {
         return entries
     }
     //: Update the journal entry
-    func update(index: Int, entry: Entry) -> Entry? {
-        if index < entries.count {
+    func update(index: Int, entry: Entry) -> Entry? {   // [6]
+        if let entry = entries.get(index: index) {
             entries[index] = entry
             return entry
-        } else {
-            return nil
-        }
+        } 
+        return nil
     }
     //: Delete a journal entry
-    func delete(index: Int) -> Entry? {
-        if index < entries.count {
+    func delete(index: Int) -> Entry? {     // [7]
+        if let _ = entries.get(index: index) {
             return entries.remove(at: index)
-        } else {
-            return nil
-        }
+        } 
+        return nil
     }
 }
+
+extension Array {
+    func get(index: Int) -> Element? {      // [4]
+        if index >= 0 && index < count {
+            return self[index]
+        }
+        return nil
+    }
+}
+
 
