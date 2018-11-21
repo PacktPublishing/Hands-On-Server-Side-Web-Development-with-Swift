2777cab3731e50eb317dab5fb8a7b040f90c0785
Angus Yeung <afyeung@gmail.com>
2018-11-21 13:30:34 -0800

    Updated package dependencies
    Updated packag dependencies to newer versions.

--- ch06_Template_Vapor/myJournal/Package.swift
+++ ch06_Template_Vapor/myJournal/Package.swift
@@ -8,10 +8,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
         // Leaf template engine
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
     targets: [
     .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
