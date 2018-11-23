af05fa6136be66ea8517a39bcb2a37772ce32c3d
Angus Yeung <afyeung@gmail.com>
2018-11-22 22:12:00 -0800

    Cleaned up the JournalEntry file
    Minor edit: removed unused code.

--- ch08_Storage_Vapor/myJournal/Sources/App/Models/JournalEntry.swift
+++ ch08_Storage_Vapor/myJournal/Sources/App/Models/JournalEntry.swift
@@ -9,7 +9,7 @@
 import FluentPostgreSQL
 import Vapor
 
-final class JournalEntry: PostgreSQLModel {//SQLiteModel {
+final class JournalEntry: PostgreSQLModel {
     var id: Int?
     var title: String
     var content: String
@@ -24,28 +24,3 @@ final class JournalEntry: PostgreSQLModel {//SQLiteModel {
 extension JournalEntry: Migration { }
 extension JournalEntry: Content { }
 extension JournalEntry: Parameter { }
-
-
-/*
-
-final class JournalEntry: Codable {
-//    typealias Database = SQLiteDatabase
-//    typealias ID = String
-//    static let idKey: IDKey = \.id
-    
-    var id: String?
-    var title: String
-    var content: String
-    
-    init(title: String,
-         content: String) {
-        self.title = title
-        self.content = content
-    }
-}
-
-extension JournalEntry: SQLiteStringModel { }
-extension JournalEntry: Migration { }
-extension JournalEntry: Content { }
-extension JournalEntry: Parameter { }
-*/
